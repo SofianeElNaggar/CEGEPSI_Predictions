318d3ce25112e5202ad611ffc4c01fcdef6bf159
SofianeElNaggar <sofiane.elnaggar@gmail.com>
2026-01-21 13:21:02 -0500

    Update infoCSV.py

--- script/gestion dataset/infoCSV.py
+++ script/gestion dataset/infoCSV.py
@@ -2,9 +2,9 @@ import csv
 import pandas as pd
 
 def count_csv_rows():
-    csv_path = "./dataset/OMDS-CTD-meteogc-data.parquet"  # <-- Mets ici le path de ton CSV
+    csv_path = "./dataset/Data Alain/OMDS-CTD-meteogc-data-v2.csv" 
 
-    df = pd.read_parquet(csv_path)
+    df = pd.read_csv(csv_path)
     print(df.shape)
 
 count_csv_rows()
